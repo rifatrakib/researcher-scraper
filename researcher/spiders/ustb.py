5f759c8fe32a1368958c9ca5bfff4b57a66aa766
rifatrakib <Rakib.1508@outlook.com>
2024-03-04 10:17:44 +0600

    structure scraping logic for members from USTB computer science faculty

--- researcher/spiders/ustb.py
+++ researcher/spiders/ustb.py
@@ -7,4 +7,21 @@ class UstbSpider(scrapy.Spider):
     start_urls = ["https://enscce.ustb.edu.cn/Teach/TeacherList/"]
 
     def parse(self, response):
-        print(f"{response.status = }")
+        sections = response.xpath("/html/body/div/div/div[2]/ul/li")
+
+        for section in sections:
+            for teacher in section.css("div.detailss > a"):
+                yield scrapy.Request(
+                    response.urljoin(teacher.attrib["href"]),
+                    callback=self.parse_teacher,
+                )
+
+    def parse_teacher(self, response, **kwargs):
+        teacher = {
+            "name": response.css("div.details_t_left > div::text").get(),
+            "title": response.css("dd:nth-child(2) > div.duty_right::text").get(),
+            "department": response.css("dd:nth-child(1) > div.duty_right::text").get(),
+            "mail": response.css("dd:nth-child(6) > div.duty_right::text").get(),
+            "research_interests": response.css("dd:nth-child(3) > div.duty_right.duty_nows::text").get(),
+        }
+        print(teacher)
